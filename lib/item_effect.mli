0409bcb98d145fc7a8ca0cbb2b224c8ef2b3081b
StephenTrainor <stephontrainor@gmail.com>
2025-07-23 18:03:15 +0000

    z

--- lib/item_effect.mli
+++ lib/item_effect.mli
@@ -1,8 +1,17 @@
+open! Core
+
 type t = {
   add_health : int;
   chance_of_adding : float;
   remove_health : int;
   chance_of_removing : float;
 }
+[@@deriving sexp]
 
-val create : ?add_health:int -> ?chance_of_adding:float -> ?remove_health:int -> ?chance_of_removing:float -> unit -> t
+val create :
+  ?add_health:int ->
+  ?chance_of_adding:float ->
+  ?remove_health:int ->
+  ?chance_of_removing:float ->
+  unit ->
+  t
